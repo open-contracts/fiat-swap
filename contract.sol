ba25d36d2bbc8324f38f86a9a86f6031375aa674
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-25 13:07:18 -0800

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,49 +1,59 @@
 pragma solidity ^0.8.0;
-
 import "https://github.com/open-contracts/protocol/blob/main/solidity_contracts/OpenContractRopsten.sol";
 
-contract FiatSwap is OpenContractAlpha {
-    
+
+contract FiatSwap is OpenContract {
     mapping(bytes32 => address) seller;
     mapping(bytes32 => address) buyer;
     mapping(bytes32 => uint256) amount;
     mapping(bytes32 => uint256) lockedUntil;
 
-
-    function secondsLeft(bytes32 offerHash) public view returns(int256) {
-        return int256(lockedUntil[offerHash]) - int256(block.timestamp);
+    constructor() {        
+        setOracle(this.buyTokens.selector, "any"); // developer mode, allows any oracleID for 'buyTokens'
     }
 
-    function ethOffered(bytes32 offerHash) public view returns(uint256) {
-        require(msg.sender == buyer[offerHash], "No ETH offered for you.");
-        return amount[offerHash];
+    // every offer has a unique offerID which can be computed with this function.
+    function offerID(string memory sellerHandle, uint256 priceInCent, string memory transactionMessage,
+                     uint8 serviceID, string memory buyerSellerSecret) public pure returns(bytes32) {
+        return keccak256(abi.encodePacked(sellerHandle, priceInCent, transactionMessage, buyerSellerSecret));
     }
 
-
-    function computeHash(string memory sellerVenmo, uint256 priceInCent, string memory transactionMessage, string memory secret) public pure returns(bytes32) {
-        return keccak256(abi.encodePacked(sellerVenmo, priceInCent, transactionMessage, secret));
+    // sellers should lock their offers, to give the buyer time to make and verify their online payment.
+    function secondsLocked(bytes32 offerID) public view returns(int256) {
+        return int256(lockedUntil[offerID]) - int256(block.timestamp);
     }
 
-    function venmoPurchase(bytes32 oracleHash, address payable msgSender, bytes32 offerHash) 
-    public _oracle(oracleHash, msgSender, this.venmoPurchase.selector) returns(bool) {
-        require(buyer[offerHash] == msgSender);
-        uint256 payment = amount[offerHash];
-        amount[offerHash] = 0;
+    // every offer has a unique offerID which can be computed with this function.
+    function weiOffered(bytes32 offerID) public view returns(uint256) {
+        require(msg.sender == buyer[offerID], "No ether offered for you at this offerID.");
+        require(secondsLocked(offerID) > 1200, "Offer isn't locked for at least 20min. Ask the seller for more time.");
+        return amount[offerID];
+    }
+
+    // to make an offer, the seller specifies the offerID, the buyer, and the time they give the buyer
+    function offerTokens(bytes32 offerID, address buyerAddress, uint256 lockForSeconds) public payable {
+        amount[offerID] = msg.value;
+        buyer[offerID] = buyerAddress;
+        lockedUntil[offerID] = block.timestamp + lockForSeconds;
+        seller[offerID] = msg.sender;
+    }
+
+    // to accept a given offerID and buy tokens, buyers have to verify their payment 
+    // using the oracle whose oracleID was specified in the constructor at the top
+    function buyTokens(bytes32 oracleID, address payable msgSender, bytes32 offerID) 
+    public checkOracle(oracleID, this.buyTokens.selector) returns(bool) {
+        require(buyer[offerID] == msgSender);
+        uint256 payment = amount[offerID];
+        amount[offerID] = 0;
         return msgSender.send(payment);
     }
 
-    function offer(address to, bytes32 offerHash, uint256 lockForSeconds) public payable {
-        amount[offerHash] = msg.value;
-        buyer[offerHash] = to;
-        lockedUntil[offerHash] = block.timestamp + lockForSeconds;
-        seller[offerHash] = msg.sender;
-    }
-    
-    function retract(bytes32 offerHash) public returns(bool) {
-        require(seller[offerHash] == msg.sender, 'only seller can retract offer');
-        require(lockedUntil[offerHash] <= block.timestamp, "can't retract offer during the locking period. check 'secondsLeft()'");
-        uint256 payment = amount[offerHash];
-        amount[offerHash] = 0;
+    // sellers can retract their offers once the time lock lapsed.
+    function retractOffer(bytes32 offerID) public returns(bool) {
+        require(seller[offerID] == msg.sender, "Only seller can retract offer.");
+        require(secondsLocked(offerID) <= 0, "Can't retract offer during the locking period.");
+        uint256 payment = amount[offerID];
+        amount[offerID] = 0;
         return payable(msg.sender).send(payment);
     }
 }
