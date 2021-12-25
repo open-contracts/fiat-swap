421a0382dd392b3a23083d157fe419a9dbd59c94
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-12-25 10:50:29 +0100

    Update oracle.py

--- venmo_oracle/oracle.py
+++ venmo_oracle/oracle.py
@@ -42,7 +42,6 @@ with opencontracts.enclave_backend() as enclave:
     assert payment >= price, f"Found {len(transactions)} payments labeled '{message}', adding up to ${payment/100} which is too low."
     return payment
   
-  enclave.open_up_domain("venmo.com")
   payment = enclave.interactive_session(url='https://venmo.com', parser=parser, instructions=instructions)
   enclave.print(f'Your total payment of ${payment/100} to {seller} was confirmed.')
   enclave.submit(offerID, types=("bytes32",), function_name="buyTokens")
